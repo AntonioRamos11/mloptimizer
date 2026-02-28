ae0a5e52b9728ac8148ab9a9c033ab5dc1f70758
Antonio Ramos <L201707889@culiacan.tecnm.mx>
2026-02-27 21:55:36 -0700

    add:test case

--- run_master.py
+++ run_master.py
@@ -2,6 +2,15 @@ import os
 # Force single GPU visibility BEFORE importing TensorFlow
 os.environ["CUDA_VISIBLE_DEVICES"] = os.getenv("CUDA_VISIBLE_DEVICES", "0")
 
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+
+for logger_name in ['aiormq', 'pika', 'aio_pika', 'asyncio', 'tensorflow', 'matplotlib']:
+    logging.getLogger(logger_name).setLevel(logging.WARNING)
+
 from app.init_nodes import *
 from app.common.dataset import *
 from app.common.search_space import *
